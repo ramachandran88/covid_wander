78cab434dbb52f2e9426ffc025c20e38c8f2b58d
Ramamurthy Ramachandran <ramamurthy.ramachandran@zalando.de>
2020-06-08 18:42:37 +0200

    inital commit

---
+++ src/main/java/com/wander/CovidWanderApplication.java
@@ -0,0 +1,13 @@
+package com.wander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidWanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidWanderApplication.class, args);
+	}
+
+}
